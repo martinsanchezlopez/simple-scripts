5d6d312ecadf26ae7aadae687a1a4e1b154e558d
Martin Sanchez Lopez <ireviive@gmail.com>
2021-02-02 19:34:52 +0100

    Fixed bug where value == 0

--- colorHex.py
+++ colorHex.py
@@ -3,14 +3,16 @@ from pynput import mouse
 import pyperclip
 
 def on_click(x, y, button, pressed):
-    if pressed and button == mouse.Button.left:      
+    if pressed and button == mouse.Button.left:
         colorHex(pyautogui.pixel(x, y))
         listener.stop()
 
 def colorHex(RGBArray):
     color = '#'
     for i in range(0,3):
-        color += hex(RGBArray[i])[2:]
+        if int(hex(RGBArray[i]), 16) == 0:
+            color+= "0" + hex(RGBArray[i])[2:]
+        else: color += hex(RGBArray[i])[2:]
     pyperclip.copy(color)
 
 with mouse.Listener(
